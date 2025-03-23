9fbae8abf48f0995997126e5200becdb87f73391
edesem <leventezoltan@protonmail.com>
2025-03-23 22:38:41 +1100

    implemented rk(x)

--- boyer-moore.py
+++ boyer-moore.py
@@ -1,25 +1,40 @@
 from collections import defaultdict
 
-def rx(s):
+def bad_char(s):
     hash = defaultdict(int)
     n = len(s)
     for i in range(1, n+1):
-        print(n-i, hash[s[-i]])
-        if hash[s[-i]] < n - i:
-            hash[s[-i]] = n - i
+        char = s[-i]
+
+        if hash[char] < n - i:
+            hash[char] = n - i
 
     return hash
 
-def rkx(s):
-    
+def extended_bad_char(s):
+    n = len(s)
+    hash = defaultdict(lambda: [0] * (n - 1))
+
+    for i in range(1, n):
+        suffix = s[:i]
+        char = suffix[i - 1]
+        print(suffix)
+
+        if hash[char][i - 1] < n - i:
+            hash[char][i - 1] = i - 1
+
+    return hash
+
         
 
 def bm(s):
     """
     Find R(x)
     """
-    rx = rx(s)
+    print(extended_bad_char(s))
 
 
+s1="acababacaba"
+s2="tbapxab"
 
-bm("acababacaba")
+bm(s2)
