42fd410bf7322041b2bb87cfb7551fdbba27cf98
Mashed <leventezoltan@protonmail.com>
2025-03-27 12:26:22 +1100

    implemented key

--- bwt.py
+++ bwt.py
@@ -5,11 +5,18 @@ def bwt(s):
     for i in range(1, len(s)):
         bwt.append(s[-i:] + s[:-i])
 
-    return bwt
+    bwt.sort()
+
+    key = ""
+
+    for i in range(len(s)):
+        print(bwt[i][-1])
+        key += bwt[i][-1]
+
+    return key
 
 s1 = "wooloomooloo"
 
 bwt_a = bwt(s1)
 
-for i in bwt_a:
-    print(i)
+print(bwt_a)
