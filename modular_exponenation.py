e38c653c79b1ed21cecd2a0fa6c3d2c35e8bb6a2
edesem <leventezoltan@protonmail.com>
2025-04-22 17:48:30 +1000

    almost correct

--- modular_exponenation.py
+++ modular_exponenation.py
@@ -1,8 +1,26 @@
 def modulo(a, b, n):
     binary = getBinaryRepresentation(b)
-    print(binary)
+    
+    # Base case
+    current = a % n
+    lsb = binary[-1]
+    binary = binary[:-1]
+    if lsb in binary == 1:
+        result = current
+    else:
+        result = 1
+
+    for i in range(1, len(binary) + 1):
+        lsb = int(binary[-i])
+        current = (current * current) % n
+        if lsb == 1:
+            result = (result * current) % n
+        return result
+    
 
 def getBinaryRepresentation(n):
+    # Reference is geeks for geeks
+    # https://www.geeksforgeeks.org/python-program-to-covert-decimal-to-binary-number/
     res = ''  # binary result
 
     while n > 0:
@@ -11,4 +29,4 @@ def getBinaryRepresentation(n):
 
     return res
 
-modulo(1, 21323, 1)
+print(modulo(7, 330, 13))
