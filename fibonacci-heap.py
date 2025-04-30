8f8346943adf5f07db1dff59be1007599657e1b7
edesem <leventezoltan@protonmail.com>
2025-04-30 23:04:00 +1000

    quick implementation of insert

--- fibonacci-heap.py
+++ fibonacci-heap.py
@@ -11,6 +11,28 @@ class Node():
 
 class FibonacciHeap():
     def __init__(self):
-        self.n = 0
+        self.count = 0
         self.min = None
         self.root_list = None
+
+    def insert(self, key):
+        node = Node(key) 
+        node.left = node
+        node.right = node
+
+        self.join_root_list(node)
+
+        if key < self.min.key:
+            self.min = node
+        
+        self.count += 1
+        return node
+
+    def join_root_list(self, node):
+        if self.root_list is None:
+            self.root_list = node
+        else:
+            node.right = self.root_list
+
+    def minimum(self):
+        return self.min
