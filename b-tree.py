4a9a93b2862c365ba38e1ba5698e3f486abc40a3
edesem <leventezoltan@protonmail.com>
2025-05-13 16:43:22 +1000

    Base case insert

--- b-tree.py
+++ b-tree.py
@@ -1,18 +1,24 @@
 class Node():
-    def __init__(self):
-        self.keys = []
+    def __init__(self, key):
+        self.keys = [key]
         self.children = None
         self.parent = None
+
+    def insert(self, key):
+        self.keys.append(key)
 
     def get_length(self):
         return len(self.keys)
     
 class Tree():
     def __init__(self):
-        pass
+        self.count = 0
 
-    def insert(self):
-        pass
+    def insert(self, key):
+        # Base case, first insertion
+        if self.count == 0:
+            node = Node(key)
+            self.count += 1
 
     def delete(self):
         pass
